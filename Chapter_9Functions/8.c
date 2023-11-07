c097a99e0573548a408d021415dfcfdb8a38b4d3
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-07 16:01:42 +0100

    Update 8.c
    layout

--- Chapter_9Functions/8.c
+++ Chapter_9Functions/8.c
@@ -1,11 +1,7 @@
-/* Chapter 6, “C Control Statements: Looping,” (Listing 6.20)
-shows a power() function that returned the result of raising 
-a type double number to a positive integer value. Improve the
-function so that it correctly handles negative powers. Also,
-build into the function that 0 to any power other than 0 
-is 0 and that any number to the 0 power is 1.(It should report 
-that 0 to the 0 is undefined, then say it’s using a value of 1.) 
-Use a loop. Test the function in a program. */
+/* Chapter 6, “C Control Statements: Looping,” (Listing 6.20) shows a power() function that returned the result of raising 
+a type double number to a positive integer value. Improve the function so that it correctly handles negative powers. Also,
+build into the function that 0 to any power other than 0 is 0 and that any number to the 0 power is 1.(It should report 
+that 0 to the 0 is undefined, then say it’s using a value of 1.) Use a loop. Test the function in a program. */
 #include <stdio.h> 
 double power (double, int);
 
